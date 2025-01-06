39335b46026f0acfce99be73149fbd1b5fc926ce
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-06 11:49:00 +0530

    create user.ts class

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+export class user{
+    email:string;
+    password:string;
+    role:string;
+
+    constructor(email: string, password: string, role: string) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
