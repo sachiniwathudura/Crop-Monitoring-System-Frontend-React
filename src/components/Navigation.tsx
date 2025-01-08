1b520f24e6eacbef4333c4d61dece455c192f90b
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-08 14:55:50 +0530

    navigation implemented

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,15 +6,16 @@ export function Navigation() {
         <>
             <header>
                 <nav className="navbar">
+                    {/*bg-[url('src/assets/ripe-rice-in-the-field-of-farmland.webp')] bg-cover bg-center h-16)*/}
                     <ul>
                         <Link to='/' className="custom-link">Dashboard</Link>
-                        <Link to='/staff' className="custom-link">staff</Link>
-                        <Link to='/field' className="custom-link">field</Link>
-                        <Link to='/monitoringlog' className="custom-link">monitoringLog</Link>
-                        <Link to='/vehicle' className="custom-link">vehicle</Link>
-                        <Link to='/crop' className="custom-link">crop</Link>
-                        <Link to='/equipment' className="custom-link">equipment</Link>
-                        <Link to='/user' className="custom-link">user</Link>
+                        <Link to='/staff' className="custom-link">Staff</Link>
+                        <Link to='/field' className="custom-link">Field</Link>
+                        <Link to='/monitoringlog' className="custom-link">MonitoringLog</Link>
+                        <Link to='/vehicle' className="custom-link">Vehicle</Link>
+                        <Link to='/crop' className="custom-link">Crop</Link>
+                        <Link to='/equipment' className="custom-link">Equipment</Link>
+                        <Link to='/user' className="custom-link">User</Link>
                     </ul>
                 </nav>
             </header>
