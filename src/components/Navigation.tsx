81e07fb7e6dfc886b72ca7cd8108c211025f4533
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-15 05:41:29 +0530

    Here’s a suitable commit message for your code: update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,10 +11,10 @@ export function Navigation() {
                         <Link to='/' className="custom-link">Dashboard</Link>
                         <Link to='/staff' className="custom-link">Staff</Link>
                         <Link to='/field' className="custom-link">Field</Link>
-                        <Link to='/monitoringlog' className="custom-link">MonitoringLog</Link>
-                        <Link to='/vehicle' className="custom-link">Vehicle</Link>
                         <Link to='/crop' className="custom-link">Crop</Link>
+                        <Link to='/vehicle' className="custom-link">Vehicle</Link>
                         <Link to='/equipment' className="custom-link">Equipment</Link>
+                        <Link to='/monitoringlog' className="custom-link">MonitoringLog</Link>
                         <Link to='/user' className="custom-link">User</Link>
                     </ul>
                 </nav>
