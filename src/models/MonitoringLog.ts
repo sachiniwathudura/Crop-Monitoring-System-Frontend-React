7dd77c0e1f49876c37440acedafd44c147cfbd8e
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-16 19:52:09 +0530

    update MonitoringLog.ts

--- src/models/MonitoringLog.ts
+++ src/models/MonitoringLog.ts
@@ -1,20 +1,27 @@
-export class MonitoringLog{
+import {Field} from "./Field";
+import {Crop} from "./Crop";
+import {Staff} from "./Staff";
+import {ReactNode} from "react";
+
+export class MonitoringLog {
     logCode: string;
-    logDate : Date;
-    logDetails : string;
-    ObservedImage : string;
-    field: string;
-    crops: string;
-    staff: string;
+    logDate: Date;
+    observation:string;
+    observedImage:string;
+    field:Array<Field>;
+    fieldCode:ReactNode;
+    crop:Array<Crop>;
+    cropCode:ReactNode;
+    staff:Array<Staff>;
+    staffId:ReactNode;
 
-
-    constructor(logCode: string, logDate: Date, logDetails: string, ObservedImage: string, field: string, crops: string, staff: string) {
+    constructor(logCode: string, logDate: Date, observation: string, observedImage: string, field:Array<Field>,crop:Array<Crop>,staff:Array<Staff>) {
         this.logCode = logCode;
         this.logDate = logDate;
-        this.logDetails = logDetails;
-        this.ObservedImage = ObservedImage;
+        this.observation = observation;
+        this.observedImage = observedImage;
         this.field = field;
-        this.crops = crops;
+        this.crop = crop;
         this.staff = staff;
     }
 }
