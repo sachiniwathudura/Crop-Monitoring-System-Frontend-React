43d05b6404c2e6488c18865cfce268bc2d170fed
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-19 01:11:59 +0530

    add - SignUp.tsx file for register

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../store/Store"
+import FormField from "../components/form/FormField";
+import { signUpUser } from "../reducers/AuthSlice";
+
+export function SignUp() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const authState = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    if (authState.isAuthenticated) {
+      navigate("/");
+    }
+  }, [authState.isAuthenticated, navigate]);
+
+  const navigateToSignIn = () => {
+    navigate("/login");
+  };
+
+  const [credentials, setCredentials] = useState({
+    name: "",
+    mobile: "",
+    username: "",
+    password: "",
+    cpassword: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(signUpUser(credentials));
+    if(!authState.error){
+      navigate("/login");
+    }
+    // Dispatch the signUpUser action with the credentials
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center flex-col text-gray-800 bg-white">
+      <div className="w-full max-w-[35rem] px-6 py-10 border-2 rounded-2xl bg-[#f5f5f5]/70 -mt-14">
+        <h1 className="text-4xl font-bold text-center mb-4 text-green-700">Sign Up</h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-center items-center py-4 gap-3 mb-3"
+        >
+          <FormField
+            title="Mobile"
+            name="mobile"
+            value={credentials.mobile}
+            handleInputChange={handleInputChange}
+            error={authState.error}
+          />
+
+          <FormField
+            title="Name"
+            name="name"
+            value={credentials.name}
+            handleInputChange={handleInputChange}
+            error={authState.error}
+          />
+
+          <FormField
+            title="Username"
+            name="username"
+            value={credentials.username}
+            handleInputChange={handleInputChange}
+            error={authState.error}
+          />
+
+          <FormField
+            title="Password"
+            name="password"
+            value={credentials.password}
+            handleInputChange={handleInputChange}
+            error={authState.error}
+          />
+
+          <FormField
+            title="Confirm Password"
+            name="cpassword"
+            id="cpassword"
+            value={credentials.cpassword}
+            handleInputChange={handleInputChange}
+            error={authState.error}
+          />
+
+          {authState.error && (
+            <div className="text-red-500 text-center">
+              <p>{authState.error}</p>
+            </div>
+          )}
+
+          <div className="flex justify-center mt-3 space-x-2 w-1/2 max-w-[20rem]">
+            <button
+              type="submit"
+              className="bg-green-500 w-full text-black-600 px-4 py-2 rounded-md hover:bg-green-700 hover:text-white focus:ring-2 focus:ring-green-900 text-lg"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <div className="flex justify-center mt-4">
+          <p>
+            Already have an account?{" "}
+            <button className="text-green-600" onClick={navigateToSignIn}>
+              Sign In
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
