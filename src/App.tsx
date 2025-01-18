032e65320c797943879c2748eed0c6442aadc57c
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-19 01:05:07 +0530

    add - signin and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 
-import './App.css'
+import './App.css';
 import {createBrowserRouter, RouterProvider} from "react-router";
 import {RootLayout} from "./components/RootLayout";
 import {Dashboard} from "./pages/Dashboard";
@@ -11,10 +10,15 @@ import {Vehicle} from "./pages/Vehicle";
 import {Crop} from "./pages/Crop";
 import {Equipment} from "./pages/Equipment";
 import {User} from "./pages/User";
+import AuthLayout from "./components/AuthLayout";
+import {SignIn} from "./pages/SignIn";
+import {SignUp} from "./pages/SignUp";
+
 
 function App() {
 
     const routes = createBrowserRouter([
+
         {
             path: '',
             element : <RootLayout/>,
@@ -29,13 +33,24 @@ function App() {
                 { path : '/user', element : <User/>},
             ]
         },
-    ])
+        {
+            path: "",
+            element: <AuthLayout />, // Separate layout for auth pages
+            children: [
+                { path: "/login", element: <SignIn /> },
+                { path: "/signup", element: <SignUp /> },
+            ],
+        },
+    ]);
 
   return (
     <>
         <RouterProvider router={routes}/>
+
     </>
   )
 }
 
-export default App
+export default App;
+
+
